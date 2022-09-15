eececb1ac0957e7af9cbe0f1bdfa6de9da593b57
Gene Kogan <kogan.gene@gmail.com>
2022-09-15 15:28:18 +0000

    auth, costs

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import datetime
-from fastapi import Request, FastAPI, Security
-from fastapi_jwt import JwtAuthorizationCredentials, JwtAccessBearer
 from pydantic import BaseModel
+from fastapi import (
+    Request, 
+    FastAPI, 
+    Security, 
+    HTTPException
+)
+from fastapi_jwt import JwtAuthorizationCredentials, JwtAccessBearer
 from pymongo import MongoClient
 import replicate
 
 from secrets import *
-
+from utils import *
 
 '''
 
@@ -62,52 +67,122 @@ db = client[db_name]
 
 
 app = FastAPI()
+
 access_security = JwtAccessBearer(secret_key="secret_key", auto_error=True)
 
+# access_security = JwtAccessBearerCookie(
+#     secret_key="secret_key",
+#     auto_error=True,
+#     access_expires_delta=timedelta(hours=1),  # custom access token valid timedelta
+#     refresh_expires_delta=timedelta(days=1),  # custom access token valid timedelta
+# )
+
+
+class AuthData(BaseModel):
+    user_id: str
+    user_type: str
+
+
+# curl -d '{"user_id": "404322488215142410", "user_type": "discord"}' -H 'Content-Type: application/json' https://app.dev.aws.abraham.fun/auth
+
 @app.post("/auth")
-def auth():
-    subject = {"username": "username", "role": "user"}
-    return {"access_token": access_security.create_access_token(subject=subject)}
+def auth(auth_data: AuthData):
+    user = {"user_type": auth_data.user_type, "user_id": auth_data.user_id}
+    access_token = access_security.create_access_token(subject=user)
+    return {"access_token": access_token}
+
+# curl -d '{"user_id": "404322488215142410", "user_type": "discord"}' -H 'Content-Type: application/json' https://app.dev.aws.abraham.fun/auth
 
 
-@app.get("/users/me")
+#curl -d '{"user_id": "404322488215142410", "user_type": "discord"}' -H 'Content-Type: application/json' -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWJqZWN0Ijp7InVzZXJfdHlwZSI6ImRpc2NvcmQiLCJ1c2VyX2lkIjoiNDA0MzIyNDg4MjE1MTQyNDEwIn0sInR5cGUiOiJhY2Nlc3MiLCJleHAiOjE2NjMyNTU2MDcsImlhdCI6MTY2MzI1NDcwNywianRpIjoiOGZhOGRmMzItYWZlZS00YjYwLTg5MzItMDJmOTFhYWNlYTM4In0.SEwS00vyXM9A9yRFugecB6JuVJmrKLMYd5_BMgtM-ms" https://app.dev.aws.abraham.fun/users/me
+
+
+@app.post("/users/me")
 def read_current_user(
     credentials: JwtAuthorizationCredentials = Security(access_security),
 ):
-    return {"username": credentials["username"], "role": credentials["role"]}
+    print("GOT IT!", credentials)
+    return "yay"
 
 
 
 #curl -d '{"generator_name": "stable-diffusion", "config": {"text_input": "an apple falls from the tree", "scale": 2.0}, "source": {"origin": "dicscord", "id": 79182}}' -H 'Content-Type: application/json' https://app.dev.aws.abraham.fun/request
 
 
+#curl -d '{"generator_name": "stable-diffusion", "config": {"text_input": "an apple falls from the tree", "scale": 2.0}}' -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWJqZWN0Ijp7InVzZXJfdHlwZSI6ImRpc2NvcmQiLCJ1c2VyX2lkIjoiNDA0MzIyNDg4MjE1MTQyNDEwIn0sInR5cGUiOiJhY2Nlc3MiLCJleHAiOjE2NjMyNTU2MDcsImlhdCI6MTY2MzI1NDcwNywianRpIjoiOGZhOGRmMzItYWZlZS00YjYwLTg5MzItMDJmOTFhYWNlYTM4In0.SEwS00vyXM9A9yRFugecB6JuVJmrKLMYd5_BMgtM-ms" -H 'Content-Type: application/json' https://app.dev.aws.abraham.fun/request
 
 
+
+
+
+def get_cost(user, generator_name, config):
+    cost = 0
+    if generator_name == 'stable-diffusion':
+        if config['mode'] == 'generate':
+            cost = 1
+        elif config['mode'] == 'interpolate':
+            cost = config['n_interpolate'] * len(config['interpolate_texts'])
+    elif generator_name == 'clipx':
+        cost = 1
+    print("COST IS!")
+    return cost
+
+
+def get_permission(user, cost):
+    user_db = db.credits.find_one(user)
+    balance = user_db['balance']
+    rate_limit_hit = False
+    if rate_limit_hit:
+        return False, 'Rate limit hit, please try again later'
+    elif cost > balance:
+        return False, 'Not enough credits remaining'
+    else:
+        return True, 'Success'
+    
 class RequestData(BaseModel):
     generator_name: str
     config: dict = {}
     source: dict = {}
 
 
+
+
+
+
+# request_creation
+# - config_: json 
+# - generator_name: str
+# - header
+# - auth: str
+# - user
+#     - source: str [discord, eth] 
+#     - id: str [discord_id, wallet]
+# - callback=None
+
 @app.post("/request")
 def request(
-    request: RequestData
+    request: RequestData,
+    credentials: JwtAuthorizationCredentials = Security(access_security)
 ):
+    user = {
+        'user_type': credentials["user_type"], 
+        'user_id': credentials["user_id"]
+    }
 
-    # request_creation
-    # - config_: json 
-    # - generator_name: str
-    # - header
-    # - auth: str
-    # - user
-    #     - source: str [discord, eth] 
-    #     - id: str [discord_id, wallet]
-    # - callback=None
+    cost = get_cost(user, request.generator_name, request.config)
 
+    print("cost is")
+    print(cost)
 
-    #is_auth = verify(auth)
-    #if not is_auth:
-    #    return {status: error, message: 403}
+    permission, reason = get_permission(user, cost)   
+
+    print("done", permission, reason) 
+
+    if not permission:
+        raise HTTPException(status_code=403, detail=str(reason))
+
+    print(permission)
+
 
     #cost = get_cost(user, generator_name, config)
     #permission, reason = get_permission(user, cost)
@@ -115,11 +190,25 @@ def request(
     #    return {status: error, message: reason}
 
     generator = generators[request.generator_name]
-    cog_name, config = generator['cog'], generator['default_config']
+    config = load_json(generator.config)
+
+    print("CONFIG 1")
+    print(config)
+
+    try:
+        config = overwrite_dict(config, request.config)
+    except ValueError as error:
+        raise HTTPException(status_code=400, detail=str(error))
+
+    print("CONFIG 2")
+    print(config)
+
+
 
     #config = overwrite_fields(config, config_)
 
-    model = replicate.models.get(cog_name)
+
+    model = replicate.models.get(generator.cog)
     version = model.versions.list()[0]
     webhook_url = f"{server_url}/model_update"
 
@@ -131,7 +220,7 @@ def request(
 
     result = db.requests.insert_one({
         'timestamp': datetime.datetime.utcnow(),
-        'cog_name': cog_name,
+        'generator': generator,
         'config': config,
         'replicate_id': prediction.id
     })
@@ -175,14 +264,3 @@ async def model_update(request: Request):
 
 def get_balance(user):
     return 5
-
-def get_permission(user, cost):
-    user_balance = get_balance(user)
-    rate_limit_hit = False  # Todo
-    if not rate_limit_hit:        
-        return False, 'Rate limit reached'
-    elif user_balance >= cost:
-        return False, 'Out of credit'
-    else:
-        #subtract credit
-        return True, 'Success'
