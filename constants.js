c10f7bec905b6486eb31f5dd8843ccaeaa9b193a
Gene Kogan <kogan.gene@gmail.com>
2022-09-27 23:02:20 +0000

    update secrets again

--- constants.js
+++ constants.js
@@ -2,11 +2,11 @@ export const PORT = 3000;
 export const SERVER_URL = process.env.SERVER_URL;
 
 export const MONGO_URL = process.env.MONGO_URL;
-export const MONGO_DB_NAME = "abraham-dev";
+export const MONGO_DB_NAME = process.env.MONGO_DB_NAME;
 export const JWT_SECRET = process.env.JWT_SECRET;
 
 export const MINIO_URL = process.env.MINIO_URL;
-export const MINIO_BUCKET = "creations-stg";
+export const MINIO_BUCKET = process.env.MINIO_BUCKET;
 export const MINIO_ACCESS_KEY = process.env.MINIO_ACCESS_KEY;
 export const MINIO_SECRET_KEY = process.env.MINIO_SECRET_KEY;
 
